eb3af23894c6447863aa103e56c82c9e1c64fdd9
KimKalura <raluca.deftu@yahoo.com>
2023-07-28 22:33:03 +0300

    alg7

---
+++ src/main/java/lc/ContainerWithMostWater.java
@@ -0,0 +1,49 @@
+package lc;
+
+public class ContainerWithMostWater {
+
+    // You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+    //Find two lines that together with the x-axis form a container, such that the container contains the most water.
+    //Return the maximum amount of water a container can store.
+    //Notice that you may not slant the container.
+
+    //Input: height = [1,8,6,2,5,4,8,3,7]
+    //Output: 49
+    //Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+    //Input: height = [1,1]
+    //Output: 1
+    
+
+    // folosim 2 pointeri: unul va incepe de la inceput, celalalt de la sfarsit
+    // maxArea -> se actualizeaza in timpul parcurgerii; tine cantitatea maxima a apei
+    // maxArea este actualizată cu valoarea maximă dintre maxArea și currentArea; maxArea va avea  întotdeauna cea mai mare valoare întâlnită până acum.
+    // daca L<R, atunci mutam  L spre centru incrementandu-l cu 1 pentru a gasi o zona mai mare
+
+    public static int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int currentArea = (right - left) * Math.min(height[left], height[right]);
+            maxArea = Math.max(maxArea, currentArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        int[] height1 = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println(maxArea(height1)); // Output: 49
+
+        int[] height2 = {1, 1};
+        System.out.println(maxArea(height2)); // Output: 1
+    }
+}
--- src/main/java/lc/TwoSum.java
+++ src/main/java/lc/TwoSum.java
@@ -12,6 +12,21 @@ public class TwoSum {
     //2->0
     //7->1
 
+
+    // 1.Cream un HashMap pentru a stoca numerele și pozițiile lor
+        // Cheie: numărul curent din tablou
+        // Valoare: indicele la care se găsește numărul curent
+    // 2.obtinem nr curent de la indicele i
+    // 3.calculăm numărul x de care avem nevoie pentru a aduna cu 'current' și a obține 'target'
+    // 4.verificăm dacă numărul x este prezent în HashMap (adică dacă am găsit deja x anterior)
+    // dacă da, înseamnă că am găsit două numere a căror sumă este egală cu 'target'
+    // 5.returnăm un hashmap cu indicii celor două numere găsite
+        // primul indice este valoarea stocată în HashMap pentru x
+        // al doilea indice este indicele curent i
+    // 6.dacă numărul x nu este prezent în HashMap, stocăm numărul 'current' în HashMap
+    // împreună cu indicele său i
+
+    //CT: O(N); CS: O(N)
     public static int[] twoSum(int[] nums, int target) {
         // prin mapa retin pozitia; mapez nr la index-unde l-am vazut
         //Time:O(n)-avem doar un for, exista n numere in array
@@ -22,12 +37,13 @@ public class TwoSum {
             // current+x=target;
             // x=target-current
             int x = target - current;
+
             if (numMap.containsKey(x)) {
                 return new int[]{numMap.get(x), i};
             }
             numMap.put(current, i);
         }
-        return null; // No solution found
+        return null; // nici o solutie gasita
     }
 
     public static void main(String[] args) {
